13925b8b369426883b4f3e72b3a6bd55acee4ac7
cnwang <cnwang406@gmail.com>
2018-09-27 11:43:30 +0800

    V25

--- stock.py
+++ stock.py
@@ -18,8 +18,12 @@ def getXrate(xrate):
 	currency[u'幣別'] = currency[u'幣別'].str.extract('\((\w+)\)')
 	for xd in xrate[0]:
 		d=(currency.loc[currency[u'幣別']==xd[0]])
-		xd[1]=d.iloc[0,1]
-		xd[2]=d.iloc[0,2]
+		if (len(d)):
+			xd[1]=d.iloc[0,1]
+			xd[2]=d.iloc[0,2]
+		else:	# no such symbol
+			xd[1]='---'
+			xd[2]='---'
 
 def getStock(par):
 	lCurrency = par[0]
@@ -28,24 +32,33 @@ def getStock(par):
 	for ls in lStock:
 		print ('getting ',ls[1])
 		s = twstock.realtime.get(ls[1])
-		
-		output = '{0} ({1}) close price is {2} ('.format(ls[1],s['info']['name'],s['realtime']['latest_trade_price'])
+		if (s['Success']):
+			output = '{0} ({1}) close price is {2} ('.format(ls[1],s['info']['name'],s['realtime']['latest_trade_price'])
 
-		dif=round(float(s['realtime']['latest_trade_price']) - float(s['realtime']['open']),2)
-		ls[0]=str(s['info']['name'])
-		ls[3]=str(dif)
-		ls[4]=str(round(float(ls[3])/float(s['realtime']['open'])*100,1))+'%'
-		if dif>0:
-			ls[6]='aa0000'
-		elif dif==0:
-			ls[6]='111111'
+			dif=round(float(s['realtime']['latest_trade_price']) - float(s['realtime']['open']),2)
+			ls[0]=str(s['info']['name'])
+			ls[3]=str(dif)
+			ls[4]=str(round(float(ls[3])/float(s['realtime']['open'])*100,1))+'%'
+			if dif>0:
+				ls[6]='aa0000'
+			elif dif==0:
+				ls[6]='111111'
+			else:
+				ls[6]='00aa00'
+			output += ' diff {0} ({1})'.format(ls[3],ls[4])	
+			print (output)
+			ls[2]=s['realtime']['latest_trade_price'][:6]
 		else:
-			ls[6]='00aa00'
-		output += ' diff {0} ({1})'.format(ls[3],ls[4])	
-		print (output)
-		ls[2]=s['realtime']['latest_trade_price'][:6]
+			utput = '{0} ({1}) not found, check again'
+			ls[0]='####'
+			ls[3]='---'
+			ls[4]='-- %'
+			ls[6]='010101'
+			output += ' diff {0} ({1})'.format(ls[3],ls[4])	
+			print (output)
+			ls[2]='----'
 	print ('-'*20)
 
 def getPrice(par):
 	getXrate(par)
-	getStock(par)
+	getStock(par)	
